6abeff80b9ae950465d4a7760ba62c0c35544926
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:30:54 +0800

    Create FilesScreen.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/ui/screens/FilesScreen.kt
@@ -0,0 +1,11 @@
+package id.antasari.p5datasaver_230104040119.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FilesScreen() {
+    // Screen untuk menyimpan dan membaca data menggunakan File Handling
+    // Contoh:
+    // - Simpan catatan ke file
+    // - Baca catatan dari file
+}
