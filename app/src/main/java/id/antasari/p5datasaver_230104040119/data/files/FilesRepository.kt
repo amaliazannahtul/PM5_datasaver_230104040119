8b81bcbfa3197563bbbc221593f0a8d91317e7a5
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:39:10 +0800

    Create FilesRepository.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/data/files/FilesRepository.kt
@@ -0,0 +1,17 @@
+package id.antasari.p5datasaver_230104040119.data.files
+
+class FilesRepository {
+
+    // Repository untuk mengelola File Handling
+    // Digunakan untuk:
+    // - Menyimpan data ke file
+    // - Membaca data dari file
+
+    fun writeFile() {
+        // Logika menulis data ke file
+    }
+
+    fun readFile() {
+        // Logika membaca data dari file
+    }
+}
