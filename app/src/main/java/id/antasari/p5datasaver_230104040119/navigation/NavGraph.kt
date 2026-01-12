7ca7077c4a51f305dc9b0b4ee0816490b2d6b740
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:26:29 +0800

    Create NavGraph.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/navigation/NavGraph.kt
@@ -0,0 +1,12 @@
+package id.antasari.p5datasaver_230104040119.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NavGraph() {
+    // Mengatur navigasi antar screen:
+    // HomeScreen
+    // SharedPrefsScreen
+    // FilesScreen
+    // AboutScreen
+}
