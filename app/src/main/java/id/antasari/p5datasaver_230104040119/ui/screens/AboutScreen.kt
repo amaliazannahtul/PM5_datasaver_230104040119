442e917c2b9cc758037e6503a8d8c12624d334ec
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:33:13 +0800

    Create AboutScreen.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/ui/screens/AboutScreen.kt
@@ -0,0 +1,9 @@
+package id.antasari.p5datasaver_230104040119.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AboutScreen() {
+    // Halaman tentang aplikasi
+    // Berisi informasi pembuat dan tujuan aplikasi
+}
