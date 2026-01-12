6f0e26040d0190754ede7bbb080882b289475fe5
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:28:03 +0800

    Create HomeScreen.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/ui/screens/HomeScreen.kt
@@ -0,0 +1,11 @@
+package id.antasari.p5datasaver_230104040119.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    // Halaman utama aplikasi Data Saver
+    // Menampilkan menu:
+    // - Shared Preferences
+    // - File Handling
+}
