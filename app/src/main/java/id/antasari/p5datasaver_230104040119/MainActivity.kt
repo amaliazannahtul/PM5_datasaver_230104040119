d0b23d5ee12de403f426c809d8bffa5781e25f6a
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:23:57 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/MainActivity.kt
@@ -0,0 +1,14 @@
+package id.antasari.p5datasaver_230104040119
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // Entry point aplikasi Praktikum 5
+        }
+    }
+}
