c95bc3f6e427501967705e9766850c85f49c3d4a
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:29:15 +0800

    Create SharedPrefsScreen.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/ui/screens/SharedPrefsScreen.kt
@@ -0,0 +1,11 @@
+package id.antasari.p5datasaver_230104040119.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SharedPrefsScreen() {
+    // Screen untuk menyimpan data menggunakan SharedPreferences
+    // Contoh data:
+    // - Nama pengguna
+    // - Email
+}
