8705c20902416f40b55b9104a1e90e3ec4b04b6b
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:25:45 +0800

    Create Destinations.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/navigation/Destinations.kt
@@ -0,0 +1,8 @@
+package id.antasari.p5datasaver_230104040119.navigation
+
+object Destinations {
+    const val HOME = "home"
+    const val SHARED_PREFS = "shared_prefs"
+    const val FILES = "files"
+    const val ABOUT = "about"
+}
