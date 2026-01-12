106b8bff5f220f61aa8d9feea0761de3a4380104
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:36:09 +0800

    Create PreferencesRepository.kt

---
+++ app/src/main/java/id/antasari/p5datasaver_230104040119/data/prefs/PreferencesRepository.kt
@@ -0,0 +1,18 @@
+package id.antasari.p5datasaver_230104040119.data.prefs
+
+class PreferencesRepository {
+
+    // Repository untuk mengelola SharedPreferences
+    // Digunakan untuk menyimpan data sederhana seperti:
+    // - Nama pengguna
+    // - Email
+    // - Pengaturan aplikasi
+
+    fun saveData() {
+        // Logika menyimpan data ke SharedPreferences
+    }
+
+    fun loadData() {
+        // Logika mengambil data dari SharedPreferences
+    }
+}
